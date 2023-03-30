8e7da957c350e6550bcc825f9f918a3df0c5bdc1
zhoupengfei <zhoupengfei@vguang.cn>
2023-03-30 16:27:20 +0800

    [feat] quickjs构建脚本，生成.a和.so，并复制到lib文件夹下

--- build.sh
+++ build.sh
@@ -1,5 +1,24 @@
-# quickjs
-cd quickjs && make
+# # quickjs
+# cd quickjs && make
 
-# lv_binding_js
-cd lv_binding_js && git submodule update --init --recursive && npm install && npm audit fix && make dev-x86
+# # lv_binding_js
+# cd lv_binding_js && git submodule update --init --recursive && npm install && npm audit fix && make dev-x86
+
+
+# 编译lvgl
+
+
+# 编译lv_drivers
+
+
+# 编译quickjs
+cp modified/quickjs/* depends/quickjs/
+cd depends/quickjs/
+make
+gcc -shared -fPIC -o libquickjs.so .obj/quickjs.o .obj/libregexp.o .obj/libunicode.o .obj/cutils.o .obj/quickjs-libc.o .obj/libbf.o
+cp libquickjs.so ../../lib/dynamic
+cp libquickjs.a ../../lib/static
+cd ../../
+
+
+
