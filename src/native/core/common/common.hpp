d9c6c92c8ba6b1ae6f5b22f81b7e6e02ccaba70e
zhoupengfei <zhoupengfei@vguang.cn>
2023-03-31 17:57:19 +0800

    [feat] dxjs原生组件公共头文件

---
+++ src/native/core/common/common.hpp
@@ -0,0 +1,12 @@
+extern "C" {
+    #include "lvgl/lvgl.h"
+    #include "quickjs/quickjs.h"
+    #include "quickjs/quickjs-libc.h"
+    #include "djs.h"
+    #include "global.h"
+};
+
+#include <vector>
+#include <string>
+#include <stdint.h>
+#include <stdlib.h>
